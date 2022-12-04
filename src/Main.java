f7e96c9282a473290ab8ee1e6b00db7952a49c67
HasanKarslioglu <86919390+HasanKarslioglu@users.noreply.github.com>
2022-12-04 17:17:30 +0300

    Added printline function
    Second commit for testing.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
 
+    public static void printline(){
+        System.out.println("Hello Github...");
+    }
+
+    public static void main(String[] args) {
+        printline();
     }
 }
